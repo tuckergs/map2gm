5126070e27bfdea4d2f4e50e0dd58b8916cd091c
Patrick Traynor <patrickjtraynor@gmail.com>
2015-06-19 22:14:38 -0700

    Small cleanup of last commit

--- convert.py
+++ convert.py
@@ -2,40 +2,34 @@ def group(lst, n):
     for i in range(0, len(lst), n):
         val = lst[i:i+n]
         if len(val) == n:
-            yield tuple(val)
+            yield val
 
-name_to_rmj = {'12':'spikeUp','9':'spikeDown','11':'spikeRight','10':'spikeLeft','2':'block','32':'save','3':'start','20':'end'}
+rmj_to_gm = {'12':'spikeUp','9':'spikeDown','11':'spikeRight','10':'spikeLeft','2':'block','32':'save','3':'start','20':'end'}
 
 rmj_filename = 'map.map'
 with open(rmj_filename) as f:
     line = f.readlines()[3]
-    entities = list(group(line.split(' ')[1:], 3))
-for i, ent in enumerate(entities):
-    entities[i] = (ent[0], ent[1], name_to_rmj[ent[2]])
-
-formatstring = '    <instance objName="%s" x="%s" y="%s" name="inst_%s" locked="0" code="" scaleX="1" scaleY="1" colour="4294967295" rotation="0"/>\n'
+    entities = list(group(line[1:].split(' '), 3))
+for ent in entities:
+    ent[2] = rmj_to_gm[ent[2]]
 
 template_filename = 'rTemplate.room.gmx'
-output_filename = 'gm/gm/rooms/rOutput.room.gmx'
+output_filename = 'gm/rooms/rOutput.room.gmx'
 
-f = open(template_filename, 'r')
-contents = f.readlines()
-f.close()
+with open(template_filename, 'r') as f:
+    lines = f.readlines()
 
 try:
-    index = contents.index('  <instances/>\n')
-    contents[index] = '  <instances>\n'
-    contents.insert(index + 1, '  </instances>\n')
+    startindex = lines.index('  <instances/>\n') + 1
+    lines[startindex - 1] = '  <instances>\n'
+    lines.insert(startindex, '  </instances>\n')
 except ValueError:
-    index = contents.index('  <instances>\n') + 1
+    startindex = lines.index('  <instances>\n') + 1
 
-print(index)
-
+formatstring = '    <instance objName="%s" x="%s" y="%s" name="inst_%s" locked="0" code="" scaleX="1" scaleY="1" colour="4294967295" rotation="0"/>\n'
 for i, ent in enumerate(entities):
     subbed = formatstring % (ent[2], ent[0], ent[1], hex(i)[2:].upper().zfill(8))
-    contents.insert(53, subbed)
+    lines.insert(startindex, subbed)
 
-f = open(output_filename, 'w')
-contents = ''.join(contents)
-f.write(contents)
-f.close()
+with open(output_filename, 'w') as f:
+    f.write(''.join(lines))
