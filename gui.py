e3c57235ec44a40fab6fabfc1ae806f2e6bcafdd
patrickgh3 <patrickjtraynor@gmail.com>
2015-06-23 16:17:00 -0700

    Refactoring and GUI tweaks

--- gui.py
+++ gui.py
@@ -15,51 +15,26 @@ def ask_path(entry, title, filetypes):
         entry.delete(0, tk.END)
         entry.insert(0, path)
         entry.xview(tk.END)
-        
-def button_go():
+def check_clicked(rmj_id):
+    print(rmj_id)
+    val = object_widgets[rmj_id][1].get()
+    if val == 1:
+        object_widgets[rmj_id][0].configure(state='normal')
+    else:
+        object_widgets[rmj_id][0].configure(state='disabled')
+def button_go(convert_command):
     rmj = entry_rmj.get()
     roomname = entry_roomname.get()
     template = entry_template.get()
     project = entry_project.get()
-    if rmj == '' or roomname == '' or template == '' or project == '':
-        messagebox.showinfo('', loc('error_top_field_empty'))
-        return
-    rmj_to_gm = {}
-    for key, val in object_entries.items():
-        text = val[0].get()
-        enabled = val[1].get()
-        if enabled:
-            if text == '':
-                messagebox.showinfo('', loc('error_object_no_name'))
-                return
-            else:
-                rmj_to_gm[key] = text
-                fn = os.path.join(os.path.split(project)[0], 'objects', text + '.object.gmx')
-                if not os.path.exists(fn):
-                    messagebox.showinfo('', loc('error_nonexistent_object') % text)
-                    return
-
-    fn = os.path.join(os.path.split(project)[0], 'rooms', roomname + '.room.gmx')
-    if os.path.exists(fn):
-        messagebox.showinfo('', loc('error_room_name_collision'))
-        return
-    try:
-        entities = convert.get_entities_from_rmj(rmj, rmj_to_gm)
-        convert.write_room(entities, roomname, project, template)
-        convert.add_room_to_project(roomname, project)
-        with open('prefs', 'w') as f:
-            f.write('template|%s\n' % template)
-            f.write('project|%s\n' % project)
-            for key, value in sorted(object_entries.items()):
-                f.write('%s|%s|%s\n' % (key, value[0].get(), value[1].get()))
-        messagebox.showinfo('', 'Successfully converted! Prefs saved.')
-    except:
-        info = sys.exc_info()
-        with open('errorlog.txt', 'w') as f:
-            f.write('Last error:\n\n%s\n%s\n%s' % info)
-        #todo: print stack trace
-        messagebox.showinfo('', loc('error_exception'))
     
+    rmj_to_gm = {}
+    for key, val in object_widgets.items():
+        rmj_to_gm[key] = (val[0].get(), val[1].get())
+    
+    result = convert_command(rmj=rmj,roomname=roomname,template=template,project=project,rmj_to_gm=rmj_to_gm)
+    messagebox.showinfo('', result)
+
 def row_askpath(labeltext, title, filetypes):
     global row
     tk.Label(root,text=labeltext).grid(row=row,column=0,sticky=tk.E)
@@ -75,102 +50,98 @@ def row_entry(labeltext):
     entry.grid(row=1,column=1,sticky=tk.EW)
     row += 1
     return entry
-def check_clicked(rmj_id):
-    val = object_entries[rmj_id][1].get()
-    if val == 1:
-        object_entries[rmj_id][0].configure(state='normal')
-    else:
-        object_entries[rmj_id][0].configure(state='disabled')
-def row_object(rmj_id, image_filename):
-    global objectrow
-    photo = tk.PhotoImage(file=image_filename)
-    w = tk.Label(root,image=photo)
-    w.photo = photo # to prevent it from being garbage collected?
-    canvas.create_window((16,0+objectrow*objectrowheight),anchor=tk.CENTER,window=w)
-    e = tk.Entry(root)
-    canvas.create_window((50,0+objectrow*objectrowheight),anchor=tk.W,window=e)
-    v = tk.IntVar()
-    c = tk.Checkbutton(root,text=loc('label_object_enabled'),variable=v,command=lambda: check_clicked(rmj_id))
-    v.set(1)
-    canvas.create_window((200,0+objectrow*objectrowheight),anchor=tk.W,window=c)
-    object_entries[rmj_id] = (e, v)
-    objectrow += 1
 
-root = tk.Tk()
-root.wm_title(loc('title'))
-img = tk.Image('photo', file='icon.png')
-root.tk.call('wm','iconphoto',root._w,img)
+def run(convert_command):
+    global root, canvas, row, objectrow, objectrowheight, object_widgets, entry_rmj, entry_roomname, entry_template, entry_project
+    root = tk.Tk()
+    root.resizable(True, True)
+    root.wm_title(loc('title'))
+    icon_image = tk.Image('photo', file='icon.png')
+    root.tk.call('wm','iconphoto',root._w,icon_image)
 
-row = 0
-entry_rmj = row_askpath(loc('label_rmj_map'), loc('open_rmj_map'), [('RMJ map', '.map'),('all files', '.*')])
-entry_roomname = row_entry(loc('label_room_name'))
-entry_template = row_askpath(loc('label_template_room'), loc('open_template_room'), [('GM:S room', '.room.gmx'),('all files', '.*')])
-entry_project = row_askpath(loc('label_project_file'), loc('open_project_file'), [('GM:S project', '.project.gmx'),('all files', '.*')])
+    row = 0
+    entry_rmj = row_askpath(loc('label_rmj_map'), loc('open_rmj_map'), [('RMJ map', '.map'),('all files', '.*')])
+    entry_roomname = row_entry(loc('label_room_name'))
+    entry_template = row_askpath(loc('label_template_room'), loc('open_template_room'), [('GM:S room', '.room.gmx'),('all files', '.*')])
+    entry_project = row_askpath(loc('label_project_file'), loc('open_project_file'), [('GM:S project', '.project.gmx'),('all files', '.*')])
 
-object_images = [('2','images/block.gif'),
-                 ('12','images/spikeup.gif'),
-                 ('11','images/spikeright.gif'),
-                 ('10','images/spikeleft.gif'),
-                 ('9','images/spikedown.gif'),
-                 ('19','images/minispikeup.gif'),
-                 ('18','images/minispikeright.gif'),
-                 ('17','images/minispikeleft.gif'),
-                 ('16','images/minispikedown.gif'),
-                 ('32','images/save.gif'),
-                 ('31','images/platform.gif'),
-                 ('23','images/water1.gif'),
-                 ('30','images/water2.gif'),
-                 ('20','images/cherry.gif'),
-                 ('27','images/hurtblock.gif'),
-                 ('28','images/vineright.gif'),
-                 ('29','images/vineleft.gif'),
-                 ]
+    object_images = [('2','images/block.gif'),
+                     ('12','images/spikeup.gif'),
+                     ('11','images/spikeright.gif'),
+                     ('10','images/spikeleft.gif'),
+                     ('9','images/spikedown.gif'),
+                     ('19','images/minispikeup.gif'),
+                     ('18','images/minispikeright.gif'),
+                     ('17','images/minispikeleft.gif'),
+                     ('16','images/minispikedown.gif'),
+                     ('32','images/save.gif'),
+                     ('31','images/platform.gif'),
+                     ('23','images/water1.gif'),
+                     ('30','images/water2.gif'),
+                     ('20','images/cherry.gif'),
+                     ('27','images/hurtblock.gif'),
+                     ('28','images/vineright.gif'),
+                     ('29','images/vineleft.gif'),
+                     ]
 
-objectrowheight = 40
-frameheight = 3 * objectrowheight
-canvasheight = (len(object_images) - 1) * objectrowheight
+    objectrowheight = 40
+    frameheight = 3 * objectrowheight
+    canvasheight = (len(object_images) - 1) * objectrowheight
 
-frame = tk.Frame(root,height=frameheight,relief=tk.GROOVE,borderwidth=2)
-frame.grid(row=row,column=0,columnspan=3,padx=5,pady=5)
-canvas = tk.Canvas(frame,width='8c',height=600,scrollregion=(0,0,0,canvasheight),yscrollincrement=objectrowheight)
-row += 1
-objectrow = 0
+    frame = tk.Frame(root,height=frameheight,relief=tk.GROOVE,borderwidth=2)
+    frame.grid(row=row,column=0,columnspan=3,padx=5,pady=5,sticky=tk.NSEW)
+    canvas = tk.Canvas(frame,scrollregion=(0,0,0,canvasheight),yscrollincrement=objectrowheight)
+    row += 1
+    objectrow = 0
 
-object_entries = {}
-for rmj_id, imagepath in object_images:
-    row_object(rmj_id, imagepath)
+    object_widgets = {}
+    for rmj_id, imagepath in object_images:
+        photo = tk.PhotoImage(file=imagepath)
+        w = tk.Label(root,image=photo)
+        w.photo = photo # to prevent it from being garbage collected?
+        canvas.create_window((16,0+objectrow*objectrowheight),anchor=tk.CENTER,window=w)
+        e = tk.Entry(root)
+        canvas.create_window((50,0+objectrow*objectrowheight),anchor=tk.W,window=e)
+        v = tk.IntVar()
+        c = tk.Checkbutton(root,text=loc('label_object_enabled'),variable=v,command=lambda rmj_id=rmj_id: check_clicked(rmj_id))
+        v.set(1)
+        canvas.create_window((200,0+objectrow*objectrowheight),anchor=tk.W,window=c)
+        object_widgets[rmj_id] = (e, v)
+        objectrow += 1
 
-vbar=tk.Scrollbar(frame,orient=tk.VERTICAL)
-vbar.pack(side=tk.RIGHT,fill=tk.Y)
-vbar.config(command=canvas.yview)
-canvas.config(height=frameheight)
-canvas.config(yscrollcommand=vbar.set)
-canvas.pack(side=tk.LEFT,expand=True,fill=tk.BOTH,pady=20)
+    vbar=tk.Scrollbar(frame,orient=tk.VERTICAL)
+    vbar.pack(side=tk.RIGHT,fill=tk.Y)
+    vbar.config(command=canvas.yview)
+    canvas.config(height=frameheight)
+    canvas.config(yscrollcommand=vbar.set)
+    canvas.pack(side=tk.LEFT,expand=True,fill=tk.BOTH,pady=20)
 
-tk.Button(root,text=loc('button_convert'),command=button_go).grid(row=row,column=1)
+    tk.Button(root,text=loc('button_convert') + '  ',command=lambda: button_go(convert_command),image=icon_image,compound=tk.RIGHT).grid(row=row,column=1,columnspan=2,sticky=tk.NSEW)
 
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=8)
-root.grid_columnconfigure(2, weight=1)
-
-if os.path.exists('prefs'):
-    with open('prefs', 'r') as f:
-        for line in f:
-            args = line[:-1].split('|')
-            args += [''] * (3 - len(args))
-            type, value, arg3 = args
-            if type == 'template':
-                entry_template.insert(0, value)
-                entry_template.xview(tk.END)
-            elif type == 'project':
-                entry_project.insert(0, value)
-                entry_project.xview(tk.END)
-            else:
-                object_entries[type][0].insert(0, value)
-                object_entries[type][1].set(int(arg3))
-                if int(arg3) == 1:
-                    object_entries[type][0].configure(state='normal')
+    root.grid_columnconfigure(0, weight=1, minsize=120)
+    root.grid_columnconfigure(1, weight=8, minsize=150)
+    root.grid_columnconfigure(2, weight=1, minsize=50)
+    root.minsize(320, 200)
+    root.geometry('320x350')
+    
+    if os.path.exists('prefs'):
+        with open('prefs', 'r') as f:
+            for line in f:
+                args = line[:-1].split('|')
+                args += [''] * (3 - len(args))
+                type, value, arg3 = args
+                if type == 'template':
+                    entry_template.insert(0, value)
+                    entry_template.xview(tk.END)
+                elif type == 'project':
+                    entry_project.insert(0, value)
+                    entry_project.xview(tk.END)
                 else:
-                    object_entries[type][0].configure(state='disabled')
-
-root.mainloop()
+                    object_widgets[type][0].insert(0, value)
+                    object_widgets[type][1].set(int(arg3))
+                    if int(arg3) == 1:
+                        object_widgets[type][0].configure(state='normal')
+                    else:
+                        object_widgets[type][0].configure(state='disabled')
+    
+    root.mainloop()
---
+++ rmj2gms.py
@@ -0,0 +1,45 @@
+import convert
+import gui
+import localize
+import os, sys
+
+def loc(key):
+    return localize.loc(key)
+
+def convert_pressed(rmj, roomname, template, project, rmj_to_gm):
+    #for key, value in sorted(rmj_to_gm.items()):
+    #    print('%s|%s|%s\n' % (key, value[0], value[1]))
+    #return 'asdf'
+    if rmj == '' or roomname == '' or template == '' or project == '':
+        return loc('error_top_field_empty')
+    for key, val in rmj_to_gm.items():
+        text = val[0]
+        enabled = val[1]
+        if enabled:
+            if text == '':
+                return loc('error_object_no_name')
+            else:
+                fn = os.path.join(os.path.split(project)[0], 'objects', text + '.object.gmx')
+                if not os.path.exists(fn):
+                    return loc('error_nonexistent_object') % text
+    fn = os.path.join(os.path.split(project)[0], 'rooms', roomname + '.room.gmx')
+    if os.path.exists(fn):
+        return loc('error_room_name_collision')
+    try:
+        entities = convert.get_entities_from_rmj(rmj, rmj_to_gm)
+        convert.write_room(entities, roomname, project, template)
+        convert.add_room_to_project(roomname, project)
+        with open('prefs', 'w') as f:
+            f.write('template|%s\n' % template)
+            f.write('project|%s\n' % project)
+            for key, value in sorted(rmj_to_gm.items()):
+                f.write('%s|%s|%s\n' % (key, value[0], value[1]))
+        return 'Successfully converted! Prefs saved.' # todo: loc this
+    except:
+        info = sys.exc_info()
+        with open('errorlog.txt', 'w') as f:
+            f.write('Last error:\n\n%s\n%s\n%s' % info)
+        #todo: print stack trace
+        return loc('error_exception')
+
+gui.run(convert_command=convert_pressed)
